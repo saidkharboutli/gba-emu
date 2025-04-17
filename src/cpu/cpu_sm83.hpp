74d287c386327cda6cac4be7b4fefa7833811b45
Said Kharboutli <said.kharboutli5@gmail.com>
2025-04-17 17:25:27 -0400

    feat: added subroutine instr; added shift instr

--- src/cpu/cpu_sm83.hpp
+++ src/cpu/cpu_sm83.hpp
@@ -1,6 +1,9 @@
 #ifndef GBA_EMU_TEST_CPU_SM83_HPP
 #define GBA_EMU_TEST_CPU_SM83_HPP
+
 #include <cstdint>
+#include <chrono>
+#include <thread>
 
 #include "../bus/bus.hpp"
 
@@ -47,6 +50,9 @@ typedef struct RegisterFile {
         uint16_t HL;
     };
 
+    /* Interrupt Master Enable Flag */
+    uint8_t IME;
+
     /* 16-bit Stack Pointer */
     uint16_t SP;
 
@@ -81,7 +87,7 @@ class SM83 {
      * FLAG HELPERS
      *************************************************************************/
     uint8_t get_flag(const uint8_t flag) const {
-        return reg.F & (static_cast<uint8_t>(1) << flag);
+        return (reg.F & (static_cast<uint8_t>(1) << flag)) >> flag;
     }
 
     void set_flag(const uint8_t flag, const uint8_t value) {
@@ -192,6 +198,100 @@ class SM83 {
     };
 
     /*************************************************************************
+    * SHIFT FUNCTION SELECTOR (0xCB + XX instructions)
+    * Select shift function based on x0-7 octal groups on opcode table
+    * TODO: Review signage for these functions and flags
+    *************************************************************************/
+    void RLC_r(const uint8_t reg_select) {
+        const uint8_t source = (this->*source_select[reg_select])();
+        const uint8_t carry = source >> 7;
+        const uint8_t res = source << 1 | source;
+
+        (this->*dest_select[reg_select])(res);
+        set_ZNHC(res == 0, 0, 0, carry);
+    }
+
+    void RRC_r(const uint8_t reg_select) {
+        const uint8_t source = (this->*source_select[reg_select])();
+        const uint8_t carry = source & 0b1;
+        const uint8_t res = source >> 1 | carry << 7;
+
+        (this->*dest_select[reg_select])(res);
+        set_ZNHC(res == 0, 0, 0, carry);
+    }
+
+    void RL_r(const uint8_t reg_select) {
+        const uint8_t source = (this->*source_select[reg_select])();
+        const uint8_t carry = source >> 7;
+        const uint8_t res = source << 1 | get_flag(FLAG_C);
+
+        (this->*dest_select[reg_select])(res);
+        set_ZNHC(res == 0, 0, 0, carry);
+    }
+
+    void RR_r(const uint8_t reg_select) {
+        const uint8_t source = (this->*source_select[reg_select])();
+        const uint8_t carry = source & 0b1;
+        const uint8_t res = source >> 1 | get_flag(FLAG_C) << 7;
+
+        (this->*dest_select[reg_select])(res);
+        set_ZNHC(res == 0, 0, 0, carry);
+    }
+
+    void SLA_r(const uint8_t reg_select) {
+        const uint8_t source = (this->*source_select[reg_select])();
+        const uint8_t carry = source >> 7;
+        const uint8_t res = source << 1;
+
+        (this->*dest_select[reg_select])(res);
+        set_ZNHC(res == 0, 0, 0, carry);
+    }
+
+    void SRA_r(const uint8_t reg_select) {
+        const uint8_t source = (this->*source_select[reg_select])();
+        const uint8_t carry = source & 0b1;
+        const uint8_t msb = source & 128;
+        const uint8_t res = source >> 1 | msb;
+
+        (this->*dest_select[reg_select])(res);
+        set_ZNHC(res == 0, 0, 0, carry);
+    }
+
+    void SRL_r(const uint8_t reg_select) {
+        const uint8_t source = (this->*source_select[reg_select])();
+        const uint8_t carry = source & 0b1;
+        const uint8_t res = source >> 1;
+
+        (this->*dest_select[reg_select])(res);
+        set_ZNHC(res == 0, 0, 0, carry);
+    }
+
+    void SWAP_r(const uint8_t reg_select) {
+        const uint8_t source = (this->*source_select[reg_select])();
+        const uint8_t res = source << 4 | source >> 4;
+
+        (this->*dest_select[reg_select])(res);
+        set_ZNHC(res == 0, 0, 0, 0);
+    }
+
+    void (SM83::*shift_select[8])(uint8_t) = {
+        &SM83::RLC_r, &SM83::RRC_r, &SM83::RL_r, &SM83::RR_r,
+        &SM83::SLA_r, &SM83::SRA_r, &SM83::SWAP_r, &SM83::SRL_r
+    };
+
+    /*************************************************************************
+    * BIT, RES, and SET FUNCTIONS (0xCB + XX instructions)
+    *************************************************************************/
+    void BIT_r(const uint8_t reg_select, const uint8_t bit_select) {
+        set_ZNHC((this->*source_select[reg_select])() >> bit_select, 0, 1, get_flag(FLAG_C));
+    }
+
+    void RES_r(const uint8_t reg_select, const uint8_t bit_select) {
+        const uint8_t res = (this->*source_select[reg_select])() & ~(static_cast<uint8_t>(1) << bit_select);
+        (this->*dest_select[reg_select])(res);
+    }
+
+    /*************************************************************************
      * LD INSTRUCTIONS
      *************************************************************************/
     void LD_rr(const uint8_t reg_select_d, const uint8_t reg_select_s) {
@@ -262,6 +362,33 @@ class SM83 {
     static void ADD_r16r16(uint16_t *regD, const uint16_t *regS) { *regD += *regS; }
 
     /*************************************************************************
+     * ROTATE INSTRUCTIONS
+     *************************************************************************/
+    void RLCA() {
+        const uint8_t carry = reg.A >> 7;
+        set_ZNHC(0, 0, 0, carry);
+        reg.A = reg.A << 1 | carry;
+    }
+
+    void RRCA() {
+        const uint8_t carry = reg.A & 0b1;
+        set_ZNHC(0, 0, 0, carry);
+        reg.A = reg.A >> 1 | carry << 7;
+    }
+
+    void RLA() {
+        const uint8_t carry = reg.A >> 7;
+        reg.A = reg.A << 1 | get_flag(FLAG_C);
+        set_ZNHC(0, 0, 0, carry);
+    }
+
+    void RRA() {
+        const uint8_t carry = reg.A & 0b1;
+        reg.A = reg.A >> 1 | get_flag(FLAG_C) << 7;
+        set_ZNHC(0, 0, 0, carry);
+    }
+
+    /*************************************************************************
      * JUMP INSTRUCTIONS
      *************************************************************************/
     void JR(const int8_t signed_offset) { reg.PC += signed_offset + 1; }
@@ -278,12 +405,93 @@ class SM83 {
 
     /*************************************************************************
      * SUBROUTINE INSTRUCTIONS
+     * TODO: SHOULD WE OFFSET JUMPS BY -1 TO COUNTER PC++ AT CYCLE START
      *************************************************************************/
-    void CALL(const uint16_t n16) {
-        bus->write(reg.SP, LOW(reg.PC + 1));
-        reg.SP++;
+    /* PUSHING INSTRUCTIONS: Always write lo, then hi */
+    void PUSH_AF() {
+        bus->write(reg.SP--, reg.F);
+        bus->write(reg.SP, reg.A);
+    }
+
+    void PUSH_r16(const uint16_t *regS) {
+        bus->write(reg.SP--, LOW(*regS));
+        bus->write(reg.SP, HIGH(*regS));
+    }
+
+    void CALL_n16(const uint16_t n16) {
+        bus->write(reg.SP--, LOW(reg.PC + 1));
         bus->write(reg.SP, HIGH(reg.PC + 1));
-        reg.PC++;
+
+        reg.PC = n16;
+    }
+
+    void CALL_cc_n16(const uint8_t flag_selector, const uint16_t n16) {
+        if (get_flag(flag_selector)) {
+            bus->write(reg.SP--, LOW(reg.PC + 1));
+            bus->write(reg.SP, HIGH(reg.PC + 1));
+
+            reg.PC = n16;
+        }
+    }
+
+    /* POPPING INSTRUCTIONS: Always read hi, then lo */
+    void POP_AF() {
+        reg.A = bus->read(reg.SP++);
+        reg.F = bus->read(reg.SP);
+    }
+
+    void POP_r16(uint16_t *regD) {
+        const uint16_t hi = bus->read(reg.SP++);
+        const uint16_t lo = bus->read(reg.SP);
+
+        *regD = hi << 8 | lo;
+    }
+
+    void RET() {
+        const uint16_t hi = bus->read(reg.SP++);
+        const uint16_t lo = bus->read(reg.SP);
+
+        reg.PC = hi << 8 | lo;
+    }
+
+    void RET_cc(const uint8_t flag_selector) {
+        if (get_flag(flag_selector)) {
+            const uint16_t hi = bus->read(reg.SP++);
+            const uint16_t lo = bus->read(reg.SP);
+
+            reg.PC = hi << 8 | lo;
+        }
+    }
+
+    void RETI() {
+        const uint16_t hi = bus->read(reg.SP++);
+        const uint16_t lo = bus->read(reg.SP);
+
+        reg.PC = hi << 8 | lo;
+
+        reg.IME = 1;
+    }
+
+    /*************************************************************************
+     * INTERRUPT INSTRUCTIONS
+     *************************************************************************/
+    void EI() {
+        reg.IME = 1;
+    }
+
+    void DI() {
+        reg.IME = 0;
+    }
+
+    void HALT() {
+        if (reg.IME) {
+            while (!bus->read(0xFF0F)) std::this_thread::sleep_for(std::chrono::milliseconds(20));
+            // handle interrupt here
+        } else if (!reg.IME && !bus->read(0xFF0F)) {
+            while (!bus->read(0xFF0F)) std::this_thread::sleep_for(std::chrono::milliseconds(20));
+        } else {
+            // do nothing, but next instruction must be read twice
+        }
     }
 
 public:
